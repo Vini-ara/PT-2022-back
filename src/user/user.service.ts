a55996ecadb803d9e128f7d7693c65f0ca5dc744
vinicius araujo <vinicius.ar02@gmail.com>
2022-11-22 07:12:44 -0300

    feat: created user service and the first user route

---
+++ src/user/user.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getUser(id: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        name: true,
+        picture: true,
+        is_admin: true,
+        archived: true,
+        pdi_updated_at: true,
+        answer: {
+          select: {
+            id: true,
+            questionId: true,
+            content: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+
+    return user;
+  }
+
+  async getUsers() {
+    const users = await this.prismaService.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        picture: true,
+        is_admin: true,
+        pdi_updated_at: true,
+      },
+    });
+
+    return users;
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const users = await this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        pdi_updated_at: new Date(),
+      },
+      select: {
+        id: true,
+        name: true,
+        picture: true,
+      },
+    });
+
+    return users;
+  }
+}
