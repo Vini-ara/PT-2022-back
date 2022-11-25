04edd17e1c95db034e6af6275e6db8ffbf7ec70f
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-11-25 18:21:08 -0300

    feat: auth module finished

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,89 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Req,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { JwtRefreshGuard } from './guard/jwt-refresh.guard';
+import { AuthRequest } from './interface/authRequest.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @Post('google')
+  @HttpCode(HttpStatus.OK)
+  async googleAuth(@Req() req: Request, @Body('token') googleToken: string) {
+    const { id, name, is_admin } = await this.authService.validateGoogleUser(
+      googleToken,
+    );
+
+    const { accessToken, expiresIn } =
+      await this.authService.generateJwtAccesToken(id, name, is_admin);
+
+    const refreshToken = await this.authService.generateJwtRefreshToken(
+      id,
+      name,
+      is_admin,
+    );
+
+    const user = await this.userService.updateRefreshToken(
+      id,
+      refreshToken.token,
+    );
+
+    req.res.setHeader('Set-cookie', refreshToken.token);
+
+    return { accessToken, expiresIn, user };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  async logout(@Req() request: AuthRequest) {
+    await this.userService.removeRefreshToken(request.user.id);
+
+    request.res.setHeader(
+      'Set-cookie',
+      'Refresh=; HttpOnly; Path=/; Max-Age=0',
+    );
+
+    return { message: 'Logged out' };
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  async refresh(@Req() request: AuthRequest) {
+    const { user } = request;
+
+    const { accessToken, expiresIn } =
+      await this.authService.generateJwtAccesToken(
+        user.id,
+        user.name,
+        user.is_admin,
+      );
+
+    const refreshToken = await this.authService.generateJwtRefreshToken(
+      user.id,
+      user.name,
+      user.is_admin,
+    );
+
+    await this.userService.updateRefreshToken(user.id, refreshToken.token);
+
+    request.res.setHeader('Set-cookie', refreshToken.cookie);
+
+    return { accessToken, expiresIn };
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [UserModule, ConfigModule],
+  imports: [UserModule, ConfigModule, PassportModule, JwtModule.register({})],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
