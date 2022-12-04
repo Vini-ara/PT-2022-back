f987819e7da4549171c0edf77847c077f692a9a4
vinicius araujo <vinicius.ar02@gmail.com>
2022-12-04 19:59:18 -0300

    fix: listen port and class validator config

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5137',
+    origin: process.env.CLIENT_URL,
     credentials: true,
-    exposedHeaders: ['set-cookie'],
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   app.use(cookieParser());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
